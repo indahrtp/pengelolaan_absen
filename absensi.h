72c3e8e97be270bde74fe0365e0e310efff0d3af
indahrtp <107259293+indahrtp@users.noreply.github.com>
2023-12-05 13:16:01 +0700

    Create absensi.h

---
+++ absensi.h
@@ -0,0 +1,8 @@
+/*--------------------------------------------------*/
+/* File			: absensi.h	    					              */
+/* Deskripsi	: Header ADT Buruh				          	*/
+/* Dibuat oleh	: Indah & Yani                  	  */
+/* Tanggal		: 5/12/2023 (revisi)				          */
+
+#ifndef absensi_h
+#define absensi_h
