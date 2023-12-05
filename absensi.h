1996f64981d0e7fb55a9e877ebb8ee4dab75a24f
Yani Rahmawati <107221750+yanirahmawati234@users.noreply.github.com>
2023-12-05 14:15:29 +0700

    Update absensi.h

--- absensi.h
+++ absensi.h
@@ -7,6 +7,15 @@
 #ifndef absensi_h
 #define absensi_h
 
+typedef struct {
+    char namaMhs[50];
+    char nimMhs[10];
+    char password[10];
+    char alamat[100];
+    char jurusan[30];
+    char prodi[30];
+} mahasiswa;
+
 /**/
 void adminView();
 
