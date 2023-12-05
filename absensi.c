321c188369f33620648df9e7ecd70dcdd018fd56
Yani Rahmawati <107221750+yanirahmawati234@users.noreply.github.com>
2023-12-05 14:21:35 +0700

    Update absensi.c

--- absensi.c
+++ absensi.c
@@ -5,3 +5,18 @@
 /* Tanggal		: 22/11/2023 (revisi)				*/
 
 #include<stdio.h>
+
+void registerStudent()
+{
+  mahasiswa data;
+  int totalData;
+  
+  printf("\n ---- Form untuk Pendaftaran Mahasiswa ----- \n\n");
+  printf("\n Masukkan Nama Mahasiswa: ");
+  scanf("%s", data.namaMhs);
+  printf("\n Masukkan NIM: ");
+  scanf("%s", data.nimMhs);
+  printf("\n Masukkan Password: ");
+  scanf("%s", data.password);
+  printf("\n Masukkan")
+}
