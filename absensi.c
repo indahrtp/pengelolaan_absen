75846970bee41044245ab92adbf3cd17810f4ca6
indahrtp <107259293+indahrtp@users.noreply.github.com>
2023-12-05 13:38:21 +0700

    Update absensi.c

--- absensi.c
+++ absensi.c
@@ -1,1 +1,7 @@
+/*--------------------------------------------------*/
+/* File			: absensi.c						                  */
+/* Deskripsi	: Header ADT Buruh					*/
+/* Dibuat oleh	: Indah	Ratu Pramudita/231511050	*/
+/* Tanggal		: 22/11/2023 (revisi)				*/
+
 #include<stdio.h>
