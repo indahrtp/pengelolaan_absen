2e2c5ad94fab149359be9a9833e104723c3530d5
indahrtp <107259293+indahrtp@users.noreply.github.com>
2023-12-05 13:37:06 +0700

    Update absensi.h

--- absensi.h
+++ absensi.h
@@ -1,8 +1,28 @@
 /*--------------------------------------------------*/
-/* File			: absensi.h	    					              */
-/* Deskripsi	: Header ADT Buruh				          	*/
-/* Dibuat oleh	: Indah & Yani                  	  */
-/* Tanggal		: 5/12/2023 (revisi)				          */
+/* File        : absensi.h	    					          */
+/* Deskripsi   : Header ADT Buruh				          	*/
+/* Dibuat oleh : Indah & Yani                  	    */
+/* Tanggal     : 5/12/2023 (revisi)				          */
 
 #ifndef absensi_h
 #define absensi_h
+
+/**/
+void adminView();
+
+/**/
+int loginAdmin();
+
+/**/
+void registerStudent();
+
+/**/
+int listStudent();
+
+/**/
+int search();
+
+/**/
+int deleteStudent();
+
+#endif
